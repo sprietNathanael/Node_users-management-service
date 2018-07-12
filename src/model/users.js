7b87b722428ac35aa2a8b4926830fc6c66c040e9
Nathanaël Spriet <nathanael.spriet@gmail.com>
2018-07-12 13:32:35 +0200

    Adds a unique constraint on unsername

--- src/model/users.js
+++ src/model/users.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
 	return sequelize.define( 'user', {
 		lastname: { type:DataTypes.STRING, allowNull:false},
 		firstname: {type:DataTypes.STRING, allowNull:false},
-		username: {type:DataTypes.STRING, allowNull:false},
+		username: {type:DataTypes.STRING, allowNull:false, unique:true},
 		password: {type:DataTypes.STRING, allowNull: false},
 		token: {type:DataTypes.STRING, allowNull: true},
 		adminPermission: {type:DataTypes.INTEGER, allowNull: false, defaultValue: 0}
