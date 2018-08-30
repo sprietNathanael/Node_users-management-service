36f26e5b3fa8fdc148b03e6ea3a0f33f2a7de5be
Nathanaël Spriet <nathanael.spriet@gmail.com>
2018-08-29 19:01:51 -0700

    Adds some parameters protections fot the userUtils class' methodes

--- src/controller/userUtils.js
+++ src/controller/userUtils.js
@@ -11,41 +11,6 @@ class UserUtils {
     }
 
     /**
-     * Check the validity of a user
-     * @param {String} lastname
-     * @param {String} firstname
-     * @param {String} username
-     * @param {String} password
-     */
-    checkUser(lastname, firstname, username, password) {
-        this.logger.debug("[Controller] Checking user : lastname=%s,firstname=%s,username=%s,password=%s", lastname, firstname, username, password);
-        let res = "";
-
-
-        if (typeof username !== "string") {
-            this.logger.debug("[Controller] username is wrong");
-            res += "username,";
-        }
-        if (typeof firstname !== "string") {
-            this.logger.debug("[Controller] firstname is wrong");
-            res += "firstname,";
-        }
-        if (typeof lastname !== "string") {
-            this.logger.debug("[Controller] lastname is wrong");
-            res += "lastname,";
-        }
-        if (typeof password !== "string") {
-            this.logger.debug("[Controller] password is wrong");
-            res += "password,";
-        }
-        res = res.slice(0, -1);
-        if (res === "") {
-            res = this.checkUserFormat(lastname, firstname, username, password);
-        }
-        return res;
-    }
-
-    /**
      * Check the format of a user
      * @param {String} lastname
      * @param {String} firstname
@@ -53,19 +18,33 @@ class UserUtils {
      * @param {String} password
      */
     checkUserFormat(lastname, firstname, username, password) {
+
+        if (typeof lastname !== "string") {
+            throw new TypeError("lastname is wrong : " + (typeof lastname) + " is not a string");
+        }
+        if (typeof firstname !== "string") {
+            throw new TypeError("firstname is wrong : " + (typeof firstname) + " is not a string");
+        }
+        if (typeof username !== "string") {
+            throw new TypeError("username is wrong : " + (typeof username) + " is not a string");
+        }
+        if (typeof password !== "string") {
+            throw new TypeError("firstname is wrong : " + (typeof firstname) + " is not a string");
+        }
+
         this.logger.debug("[Controller] Checking format user : lastname=%s,firstname=%s,username=%s,password=%s", lastname, firstname, username, password);
         let res = "";
-        if (username !== undefined && !this.checkUsername(username)) {
-            this.logger.debug("[Controller] username is wrong");
-            res += "username,";
+        if (lastname !== undefined && !this.checkName(lastname)) {
+            this.logger.debug("[Controller] lastname is wrong");
+            res += "lastname,";
         }
         if (firstname !== undefined && !this.checkName(firstname)) {
             this.logger.debug("[Controller] firstname is wrong");
             res += "firstname,";
         }
-        if (lastname !== undefined && !this.checkName(lastname)) {
-            this.logger.debug("[Controller] lastname is wrong");
-            res += "lastname,";
+        if (username !== undefined && !this.checkUsername(username)) {
+            this.logger.debug("[Controller] username is wrong");
+            res += "username,";
         }
         if (password !== undefined && !this.checkPassword(password)) {
             this.logger.debug("[Controller] password is wrong");
@@ -80,6 +59,9 @@ class UserUtils {
      * @param {String} name
      */
     checkName(name) {
+        if (typeof name !== "string") {
+            throw new TypeError((typeof name) + " is not a string");
+        }
         return (name.match(/^[a-zA-ZÀ-ÿ-_]+$/) != null);
     }
 
@@ -88,6 +70,9 @@ class UserUtils {
      * @param {String} username
      */
     checkUsername(username) {
+        if (typeof username !== "string") {
+            throw new TypeError((typeof name) + " is not a string");
+        }
         return (username.match(/^[a-zA-Z0-9-_]+$/) != null);
     }
 
@@ -96,6 +81,10 @@ class UserUtils {
      * @param {String} password
      */
     checkPassword(password) {
+
+        if (typeof password !== "string") {
+            throw new TypeError((typeof name) + " is not a string");
+        }
         return (password !== "" && password.length >= 8);
     }
 }
